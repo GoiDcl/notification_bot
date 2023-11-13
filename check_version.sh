aeac22e0a094137c59747a6446294c0ca5d79ccf
Шалейников Евгений <shaleinikove@krasrm.com>
2023-11-13 15:38:45 +0700

    always actual soft version

---
+++ check_version.sh
@@ -0,0 +1,7 @@
+source .env
+while
+        rm log.log
+        wget -O log.log http://webgit.krasrm.com/api/v4/projects/33/repository/files/log.log/raw?token=$GITLAB_TOKEN
+        sleep 300
+        do true;
+done
--- rmcdevbot.py
+++ rmcdevbot.py
@@ -3,6 +3,7 @@ from telegram.ext import Updater, CommandHandler
 from dotenv import load_dotenv
 import os
 import logging
+import subprocess
 from logging.handlers import RotatingFileHandler
 
 load_dotenv()
@@ -49,7 +50,17 @@ def wake_up(update, context):
     )
 
 
+def check_version():
+        try:
+            subprocess.Popen(["bash", "./check_version.sh"])
+        except OSError as e:
+            logger.error(e)
+
+
 def main():
+
+    check_version()
+
     updater = Updater(token=BOT_TOKEN)
 
     updater.dispatcher.add_handler(CommandHandler('start', wake_up))

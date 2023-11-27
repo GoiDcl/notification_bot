2a60e842ffb6725113f8e426daf1ea23a7146c4f
Шалейников Евгений <shaleinikove@krasrm.com>
2023-11-27 16:42:05 +0700

    fixed .sh script

--- check_version.sh
+++ check_version.sh
@@ -1,7 +1,7 @@
 source .env
-while
+while true
+do
         rm log.log
         wget -O log.log http://webgit.krasrm.com/api/v4/projects/33/repository/files/log.log/raw?token=$GITLAB_TOKEN
         sleep 300
-        do true;
 done
